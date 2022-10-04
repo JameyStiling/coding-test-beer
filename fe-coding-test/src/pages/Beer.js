732400b278e0937765fb6f365c311b8489a9dfb0
jastiling <jastiling@gmail.com>
2022-10-04 10:45:08 -0700

    add google fonts and make simple page struture

---
+++ fe-coding-test/src/pages/Beer.js
@@ -0,0 +1,15 @@
+// import BeerList from "./components/BeerList.js";
+import "./beer.css";
+
+function BeerPage() {
+  return (
+    <>
+      <header>
+        <h1 className="beer-title">Punk API Beers</h1>
+      </header>
+      <section>{/* <BeerList /> */}</section>
+    </>
+  );
+}
+
+export default BeerPage;
